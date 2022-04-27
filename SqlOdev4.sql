01a1cf562861d283e385ea811fd038d83c16aa41
Kasim Gokcul <90607952+xviaver@users.noreply.github.com>
2022-04-28 01:51:33 +0300

    4.Sql  Harf duzeltmesi yapildi

--- SqlOdev4.sql
+++ SqlOdev4.sql
@@ -20,4 +20,4 @@ WHERE country LIKE '_____';
 --5.city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
 
 SELECT COUNT(*) FROM city
-WHERE city ILIKE 'T%';
+WHERE city ILIKE 'R%';
